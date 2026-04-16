3d86c48566003eab05ae26738610edf1e1a13745
Rasulbek <yumyumcartoons@gmail.com>
2026-04-16 13:48:03 +0500

    adewssdadsada

---
+++ Notebook/app.py
@@ -0,0 +1,25 @@
+import streamlit as st 
+import requests
+
+api_key = "6b610bda22b1425d96a9d79608780b45"
+
+st.title("Yangiliklar Oynasi")
+
+query = st.text_input("Search news", "AI")
+
+
+if st.button("Yangiliklarni qidirish"):
+    url = "https://newsapi.org/v2/everything"
+
+    params = {
+        "q": query,
+        "apiKey" : api_key
+    }
+
+    res = requests.get(url, params=params)
+    data = res.json()
+
+    for article in data["articles"][:5]:
+        st.write("###", article["title"])
+        st.write(article["source"]["name"])
+        st.write(article["url"])
